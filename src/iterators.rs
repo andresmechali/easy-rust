e523da6898667b638fb692d29df92d1826f22fb7
Andres Mechali <andresmechali@gmail.com>
2023-06-21 13:33:00 -0300

    use zip method

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,9 +1,25 @@
+use std::collections::HashMap;
+
 pub fn run() {
     let mut doubler = Doubler::default();
 
     while let Some(current_value) = doubler.next() {
         println!("{:?}", current_value);
     }
+
+    let some_numbers = vec![0, 1, 2, 3, 4, 5];
+    let some_words = vec!["zero", "one", "two", "three", "four"];
+
+    let number_word_hashmap = some_numbers
+        .into_iter()
+        .zip(some_words.into_iter())
+        .collect::<HashMap<i32, &str>>();
+
+    println!(
+        "key: {}, value: {}",
+        1,
+        number_word_hashmap.get(&1).unwrap()
+    );
 }
 
 impl Default for Doubler {
