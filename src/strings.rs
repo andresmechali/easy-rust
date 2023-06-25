392edf1bb588beae7677c4f7e6be43794ddd5365
Andres Mechali <andresmechali@gmail.com>
2023-06-25 19:39:28 -0300

    101: named lifetimes

--- src/strings.rs
+++ src/strings.rs
@@ -5,3 +5,15 @@ pub fn run() {
 fn return_static_reference() -> &'static str {
     "static string"
 }
+
+struct City<'citylifetime> {
+    name: &'citylifetime str,
+    date_founded: u32,
+}
+
+fn returns_references<'a>(input_a: &'a str, input_b: &'a str) -> &'a str {
+    if input_a.len() > 2 {
+        return input_a;
+    }
+    input_b
+}
