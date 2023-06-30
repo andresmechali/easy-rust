20e324625ebbfbec4e20775a94029169ca787220
Andres Mechali <andresmechali@gmail.com>
2023-06-30 17:14:15 +0200

    109: more on mutex

--- src/multithread.rs
+++ src/multithread.rs
@@ -12,4 +12,32 @@ pub fn run() {
     let my_mutex_2 = Mutex::new(1);
     *my_mutex_2.lock().unwrap() = 2;
     println!("my_mutex_2: {:?}", my_mutex_2); // Mutex { 2 }
+
+    let my_book_1 = Book {
+        name: Mutex::new("Name before"),
+        author: Mutex::new("Author before"),
+        sold: Mutex::new(100_000),
+    };
+
+    let mutex_changer = my_book_1.author.lock();
+    drop(mutex_changer);
+
+    if let Ok(mut mutex) = my_book_1.author.try_lock() {
+        *mutex = "Author after";
+    }
+
+    println!("my_book_1: {:?}", my_book_1);
+
+    for _ in 0..100 {
+        *my_book_1.sold.lock().unwrap() += 1;
+    }
+
+    println!("my_book_1 sold: {:?}", my_book_1.sold);
+}
+
+#[derive(Debug)]
+struct Book<'a> {
+    name: Mutex<&'a str>,
+    author: Mutex<&'a str>,
+    sold: Mutex<u32>,
 }
